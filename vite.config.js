5a7b7b8aaed88fceb11b5abc309a6ccde6669893
kaouther <baramakaouther@gmail.com>
2025-06-05 04:39:15 -0700

    cd1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/text-analyze/',
   plugins: [react(), tailwindcss(),],
-  root: './',
+  root: './src',
   build: {
     outDir: 'build',
   },
